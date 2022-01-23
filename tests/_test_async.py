0c0c54d0814a35ea07ca4bfd3d5080a62d44f6ea
Theoden <rohanbulusu@gmail.com>
2022-01-23 11:56:43 -0700

    removed @coroutine decorators, deprecated since Python3.8

--- tests/_test_async.py
+++ tests/_test_async.py
@@ -9,13 +9,11 @@ import blinker
 def test_send_async():
     calls = []
 
-    @asyncio.coroutine
-    def receiver_a(sender):
+    async def receiver_a(sender):
         calls.append(receiver_a)
         return 'value a'
 
-    @asyncio.coroutine
-    def receiver_b(sender):
+    async def receiver_b(sender):
         calls.append(receiver_b)
         return 'value b'
 
@@ -28,8 +26,7 @@ def test_send_async():
     sig.connect(receiver_b)
     sig.connect(receiver_c)
 
-    @asyncio.coroutine
-    def collect():
+    async def collect():
         return sig.send_async()
 
     loop = asyncio.get_event_loop()
