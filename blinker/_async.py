0174b3004e46c6f57e30d6fbad017c9f0e779896
Theoden <rohanbulusu@gmail.com>
2022-01-23 12:02:31 -0700

    renamed _async.schedule to _async.create_task

--- blinker/_async.py
+++ blinker/_async.py
@@ -3,21 +3,21 @@ import asyncio
 from blinker.base import Signal
 
 
-try:
-    schedule = asyncio.create_task
-except AttributeError:
-    schedule = asyncio.ensure_future
+try:  # for Python3.7+
+    create_task = asyncio.create_task
+except AttributeError:  # for versions prior to Python3.7
+    create_task = asyncio.ensure_future
 
 
 async def _wrap_plain_value(value):
-    """Pass through a coroutine *value* or wrap a plain value."""
+    """Pass through a coroutine 'value' or wrap a plain value"""
     if asyncio.iscoroutine(value):
         value = await value
     return value
 
 
 def send_async(self, *sender, **kwargs):
-    return [(receiver, schedule(_wrap_plain_value(value)))
+    return [(receiver, create_task(_wrap_plain_value(value)))
             for receiver, value
             in self.send(*sender, **kwargs)]
 
